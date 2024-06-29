bfb2f8be0ef4f03f1977ab9ae3660a98199ab70c
Bhoumic Garg <bhoumicgarg04@gmail.com>
2024-06-29 14:32:03 +0530

    Updating security checks

--- saloonweb/settings.py
+++ saloonweb/settings.py
@@ -30,7 +30,7 @@ STATICFILES_DIRS = (
 SECRET_KEY = 'django-insecure-lsnal5g08l#m5jeqp7!(&zr0u@8aa-!i-jl9o9ak@zff!02ei!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['bhoumic-app-3c37b115b439.herokuapp.com', 'localhost', '127.0.0.1']
 
