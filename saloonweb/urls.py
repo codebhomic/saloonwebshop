6a643c7434af6d5f4904c6f5b7d1faff63260b62
Bhoumic Garg <bhoumicgarg04@gmail.com>
2024-07-01 11:50:12 +0530

    updating media files to be serving

--- saloonweb/urls.py
+++ saloonweb/urls.py
@@ -33,8 +33,8 @@ urlpatterns = [
     path('users/',include('users.urls')),
     path('',include('booking.urls')),
 ]
-# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # Serve media files during development
 if settings.DEBUG:
